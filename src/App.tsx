54c063736f2e4a07b8b115cc3d63b7bc04aaa54e
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-11-30 12:25:13 +0200

    added HelmetProvider and BackgroundGrunge

--- src/App.tsx
+++ src/App.tsx
@@ -10,41 +10,42 @@ import rectangle from "./assets/images/rectangle.png"
 import line1 from "./assets/images/line1.png"
 import line2 from "./assets/images/line2.png"
 import line3 from "./assets/images/line3.png"
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <Background>
-      <img className='grunge' src={grunge1} alt="grunge" />
-      <img className='grunge' src={grunge2} alt="grunge" />
-      <Wrapper>
-        <img className='line1' src={line1} alt="line" />
-        <img className='line2' src={line2} alt="line" />
-        <img className='line3' src={line3} alt="line" />
-        <UserContextProvider>
-          <img className='rectangle' src={rectangle} alt="rectangle" />
-          <Header />
-          <Body />
-          <Footer />
-        </UserContextProvider>
-      </Wrapper>
-    </Background >
+    <HelmetProvider>
+      <Background>
+        <BackgroundGrunge />
+        <Wrapper>
+          <img className='line1' src={line1} alt="line" />
+          <img className='line2' src={line2} alt="line" />
+          <img className='line3' src={line3} alt="line" />
+          <UserContextProvider>
+            <img className='rectangle' src={rectangle} alt="rectangle" />
+            <Header />
+            <Body />
+            <Footer />
+          </UserContextProvider>
+        </Wrapper>
+      </Background >
+    </HelmetProvider>
   );
 }
 
 const Background = styled.div`
   background-color: #e0e0e0;
-  width: 100vw;
-  height: 100vh;
   position: relative;
   overflow-x: hidden;
-
-  .grunge{
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-  }
   `
+
+const BackgroundGrunge = styled.div`
+  background-image: url(${grunge1}), url(${grunge2});
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`
 
 const Wrapper = styled.div`
     max-width: 1200px;
@@ -63,8 +64,8 @@ const Wrapper = styled.div`
 
     .rectangle{
         position: absolute;
-        width: 185px; //545px;
-        left: -10%;
+        width: 185px;
+        left: -50px;
         top: 50%  ;
         rotate: 90deg;
     }
@@ -77,7 +78,7 @@ const Wrapper = styled.div`
 
     .line2{
       width:  600px;
-      top: 150px;
+      top: 100px;
       right: -450px;
     }
 
