40264986eb3137716c6bdd0c692579261781be2d
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-12-04 19:25:23 +0200

    added Engagement component

---
+++ src/modules/atoms/Engagement.tsx
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { Instruction } from '../Body/modules/common.styled'
+import { DataContext } from '../../provider/Data'
+
+type Props = {}
+
+function Engagement({ }: Props) {
+    const { data } = useContext(DataContext);
+
+    let { comments, selectedInstagramAccount } = data;
+
+    return (
+        <Wrapper>
+            <Instruction>Engagement</Instruction>
+            <BoxContent>
+                <Box>
+                    <Title><i>Engagement rate</i></Title>
+                    <Count>{((comments.length / +selectedInstagramAccount.followers_count) * 100).toFixed(1)} %</Count>
+                </Box>
+            </BoxContent>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    margin-bottom: 20px;
+    border-left: 7px solid black;
+`
+const BoxContent = styled.div`
+    display: flex;
+    justify-content: center;
+
+ `
+
+const Box = styled.div`
+    width: 30%;
+    text-align: center;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const Title = styled.div`
+    margin-bottom: 7px;
+`
+
+const Count = styled.div`
+    font-size: 18px;
+`
+
+export default Engagement
