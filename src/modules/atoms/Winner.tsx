03ac3b250d29678851bcc1f6e2b251ef71615eec
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-12-05 13:44:26 +0200

    added Winner component

---
+++ src/modules/atoms/Winner.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+type Props = {
+    number?: number,
+    name: string,
+    text: string,
+}
+
+function Winner({ number, name, text }: Props) {
+    return (
+        <Wrapper>
+            <WinnerBox>
+                {number ? <Number># {number}</Number> : ''}
+                <Name to={`https://instagram.com/${name}`}>@{name}</Name>
+                <Text>{text}</Text>
+            </WinnerBox>
+        </Wrapper>
+    )
+};
+
+const Wrapper = styled.div`
+    box-sizing: border-box;
+    background-color: rgb(25, 25, 25);
+    color: white;
+    width: 200px;
+    height: 280px;
+    border-radius: 5px;
+
+    :hover {
+        background-color: #191919bc;
+    }
+`
+
+const WinnerBox = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    z-index: 2;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    padding: 12px;
+    align-items: center;
+    justify-content: center;
+    font-family: "Work Sans";
+    font-size: 18px;
+`
+
+const Number = styled.div`
+    font-size: 22px;
+    color: white;
+    font-weight: 600;
+    margin: 5px;
+    text-align: center;
+`
+
+const Name = styled(Link)`
+    text-decoration: none;
+    color: white;
+    font-weight: 600;
+    margin: 5px;
+    text-align: center;
+`
+
+const Text = styled.div`
+    margin-top: 4px;
+    font-family: "Open Sans";
+    font-size: 14px;
+`
+
+export default Winner
