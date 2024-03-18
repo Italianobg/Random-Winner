fb6dec2ae184c37ef69dca375e35f6c7c3f1d8fc
Ivan Petkov <ivan.slv.petkov@gmail.com>
2024-03-18 10:52:10 +0200

    Updated version of FB API

--- src/api/axiosClient.tsx
+++ src/api/axiosClient.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    baseURL: `https://graph.facebook.com/v16.0`,
+    baseURL: `https://graph.facebook.com/v19.0`,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
